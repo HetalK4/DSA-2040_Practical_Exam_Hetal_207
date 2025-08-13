869cef20ce09a667d15504c0857384e23005551b
HetalK4 <hetalkumbharana2004@gmail.com>
2025-08-13 15:45:50 +0300

    Updated Section 1 Task !

--- SECTION_1_Task_1/schema.sql
+++ SECTION_1_Task_1/schema.sql
@@ -1,10 +1,8 @@
 
 -- Dimension: Date
-CREATE TABLE dim_date (
-    date_id INT PRIMARY KEY,
+CREATE TABLE dim_time (
+    time_id INT PRIMARY KEY,
     full_date DATE,
-    day INT,
-    month INT,
     quarter INT,
     year INT
 );
@@ -13,27 +11,25 @@ CREATE TABLE dim_date (
 CREATE TABLE dim_product (
     product_id INT PRIMARY KEY,
     name VARCHAR(100),
-    category VARCHAR(50),
-    brand VARCHAR(50)
+    category VARCHAR(50)
 );
 
--- Dimension: Store
-CREATE TABLE dim_store (
-    store_id INT PRIMARY KEY,
-    store_name VARCHAR(100),
-    city VARCHAR(50),
-    region VARCHAR(50)
+-- Dimension: Customer
+CREATE TABLE dim_customer (
+    customer_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    location VARCHAR(50)
 );
 
 -- Fact: Sales
 CREATE TABLE fact_sales (
     sale_id INT PRIMARY KEY,
-    date_id INT,
+    time_id INT,
     product_id INT,
-    store_id INT,
+    customer_id INT,
     quantity_sold INT,
     revenue DECIMAL(10, 2),
-    FOREIGN KEY (date_id) REFERENCES dim_date(date_id),
+    FOREIGN KEY (time_id) REFERENCES dim_time(time_id),
     FOREIGN KEY (product_id) REFERENCES dim_product(product_id),
-    FOREIGN KEY (store_id) REFERENCES dim_store(store_id)
+    FOREIGN KEY (customer_id) REFERENCES dim_customer(customer_id)
 );
